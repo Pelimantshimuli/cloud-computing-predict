71b1fbd799f5a090f43bb771fac2892cb4415afa
Pelimantshimuli <125760895+Pelimantshimuli@users.noreply.github.com>
2024-04-29 00:10:35 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://dlibwkv984.execute-api.eu-west-1.amazonaws.com/2401FTDE-PREMAN-CC-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
