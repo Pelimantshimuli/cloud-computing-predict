68d3660225e4c5896400dd16cf5ea094d035b5a5
Pelimantshimuli <125760895+Pelimantshimuli@users.noreply.github.com>
2024-05-03 01:55:36 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://dlibwkv984.execute-api.eu-west-1.amazonaws.com/2401FTDE-PREMAN-CC-predict-lambda",
+                url: "https://3igmrt4k13.execute-api.eu-west-2.amazonaws.com/2401FTDE-PREMAN-CC-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
